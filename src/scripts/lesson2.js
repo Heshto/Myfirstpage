d143dc0d3233995f33fcad1558fe87db3cd775ff
air13 <air13@MacBook-Air-air13.local>
2022-11-03 14:17:13 +0300

    Внес все изменения

---
+++ src/scripts/lesson2.js
@@ -0,0 +1,8 @@
+"use strict";
+// Это означает что код работает в современном
+let message;
+message = "Hello";
+// Создал переменную и присвоил ей значение hello
+alert(message);
+let message2 = "пидр";
+alert(message2);
