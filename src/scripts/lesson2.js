99511468d789f6d4d837d60e6c69fe1164646f71
air13 <air13@MacBook-Air-air13.local>
2022-11-07 20:12:02 +0400

    Добавил отрисованный плюсик, заменил рейтин, поправил размеры странички макс и мин, подчистил ненужные батоны, сделал подсветку имг в асайде, поправил в слейве скачки батонов.

--- src/scripts/lesson2.js
+++ src/scripts/lesson2.js
@@ -4,5 +4,35 @@ let message;
 message = "Hello";
 // Создал переменную и присвоил ей значение hello
 alert(message);
-let message2 = "пидр";
+let message2 = "mtfckr";
 alert(message2);
+const ratings = document.querySelectorAll('.rating')
+// Проверяем на наличие рэйтингов и запускаем функцию
+if (ratings.lenght > 0) {
+  initRatings()
+}
+// Основная функция
+function initRatings() {
+  let ratingActive, ratingValue;
+  // смотрим все рейтинги на странице
+  for (var i = 0; i < ratings.length; i++) {
+    const rating = ratings[i];
+    initRating(rating);
+  }
+  // Подрубаем нужный рейтинг
+  function initRating(rating) {
+    initRatingVars(rating);
+
+    SetRatingActiveWidth();
+  }
+  // инициализация переменных
+  function initRatingVars(rating) {
+    ratingActive = rating.querySelector('.rating_active');
+    ratingValue = rating.querySelector('.rating_value');
+  }
+  // Изменение ширины активных звезд
+  function SetRatingActiveWidth(i = ratingValue.innerHTML) {
+    const ratingActiveWidth = i / 0.05;
+    ratingActive.style.width = `${ratingActiveWidth}%`;
+  }
+}
